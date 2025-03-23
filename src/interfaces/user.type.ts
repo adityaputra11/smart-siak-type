f821d262443d4812403efbb66469152580a5ba1d
Aditya <aditya.pratama@akarinti.tech>
2025-03-23 14:32:20 +0700

    feat: add lecturer

--- src/interfaces/user.type.ts
+++ src/interfaces/user.type.ts
@@ -1,11 +1,21 @@
+
+
+
 export enum UserRole {
   ADMIN = 'admin',
-  USER = 'user'
+  STUDENT = 'student',
+  LECTURER = 'lecturer',
+  STAFF = 'staff',
 }
 
 export interface User {
   id: string;
-  name: string;
+  username: string;
   email: string;
-  role: UserRole
+  password: string;
+  fullName: string;
+  role: UserRole;
+  isActive: boolean;
+  createdAt: Date;
+  updatedAt: Date;
 }
