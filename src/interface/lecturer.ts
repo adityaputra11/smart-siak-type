e7fbd72b6a5eb762601090fa4418ce15e4094214
Aditya <aditya.pratama@akarinti.tech>
2025-03-26 19:47:53 +0700

    fix: type lecturer

--- src/interface/lecturer.ts
+++ src/interface/lecturer.ts
@@ -1,6 +1,7 @@
 import { BaseStatus, BaseType } from "./basetype";
 import { Subject } from "./subject";
 import { ClassSchedule } from "./class_schedule";
+import { Department } from "./department";
 
 export type LecturerStatus = typeof BaseStatus;
 export interface Lecturer extends BaseType {
@@ -8,7 +9,8 @@ export interface Lecturer extends BaseType {
   nip: string;
   email: string;
   phone: string;
-  department: string;
+  department_id: string;
+  department: Department;
   specialization: string[];
   user_id: string;
   subjects: Subject[];
